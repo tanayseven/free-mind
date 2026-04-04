4dc8ed43b6e3d0d2935d03e1c0fd26623c74e1aa
Tanay PrabhuDesai <contact@tanay.tech>
2026-04-04 18:03:02 +0530

    Inline header into page, add dark mode toggle and status indicator

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Header from './Header.svelte';
 	import Footer from './Footer.svelte';
 	import '../app.css';
 
@@ -7,9 +6,7 @@
 </script>
 
 <div class="app min-h-screen flex flex-col">
-	<Header />
-
-	<main class="flex-1 flex flex-col items-center justify-center p-4 md:p-6 lg:p-8">
+	<main class="flex-1 flex flex-col">
 		{@render children()}
 	</main>
 
